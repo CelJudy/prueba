c985203d67ca4ccea06a4f8340d83dae620041ca
CelJudy <celjudygmail.com>
2026-06-29 11:52:00 -0600

    primer commit

---
+++ src/asesoria/Asesoria.java
@@ -0,0 +1,49 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package asesoria;
+
+/**
+ *
+ * @author celju2
+ */
+public class Asesoria {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //imprimir();
+        
+        Clase1 objClase1=new Clase1();
+        objClase1.imprimir("hola jesi");
+        
+        Clase3 objClase3=new Clase3();
+        objClase3.entero=5;
+        objClase1.imprimir(Integer.toString(objClase3.entero));
+        
+        Clase3 objClase3v2=new Clase3();
+        objClase3v2.entero=10;
+        
+        objClase3.sumar(objClase3.entero, objClase3v2.entero);
+        
+        
+    }
+    
+    static public void imprimir(){
+        System.out.println("Hola mundo");
+    }
+    
+    static public String concatenar(){
+        return "Hola mundo";
+    }
+    
+    static public int sumar(int numero1){
+        return numero1++;
+    }
+    static public int restar(int numero1){
+        return numero1--;
+    }
+    
+}
---
+++ src/asesoria/Clase3.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package asesoria;
+
+/**
+ *
+ * @author celju2
+ */
+public class Clase3 {
+    
+    public String texto;
+    public int entero;
+    
+    public int sumar(int numero1, int numero2){
+        return numero1+numero2;
+    }
+    public int restar(int numero1, int numero2){
+        return numero1-numero2;
+    }
+}
